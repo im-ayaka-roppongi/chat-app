a1cb2c1004608def8be0a9fd035247a15a569ff4
im-ayaka-roppongi <a.roppongi@infomart.co.jp>
2025-11-25 11:35:23 +0900

    ユーザー編集機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/UserController.java
+++ src/main/java/in/tech_camp/chat_app/controller/UserController.java
@@ -4,15 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import in.tech_camp.chat_app.entity.UserEntity;
-import in.tech_camp.chat_app.form.UserForm;
 import in.tech_camp.chat_app.form.LoginForm;
+import in.tech_camp.chat_app.form.UserEditForm;
+import in.tech_camp.chat_app.form.UserForm;
 import in.tech_camp.chat_app.repository.UserRepository;
 import in.tech_camp.chat_app.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -64,5 +66,42 @@ public class UserController {
       model.addAttribute("loginError", "Invalid email or password.");
     }
       return "users/login";
+  }
+
+  // 編集画面を表示する
+  @GetMapping("/users/{userId}/edit")
+  // pathに含まれるuserIdを取得するために@PathVariableを使う。ここで処理した内容はビューで表示するのでモデルオブジェクトも渡す
+  public String editUserForm(@PathVariable("userId") Integer userId, Model model) {
+    // 取得したidのuser情報をとってきてuserに格納する（idでdb検索するメソッドはリポジトリに記載）
+    UserEntity user = userRepository.findById(userId);
+    
+    UserEditForm userForm = new UserEditForm();
+    userForm.setId(user.getId());
+    userForm.setName(user.getName());
+    userForm.setEmail(user.getEmail());
+
+    model.addAttribute("user", userForm);
+    return "users/edit";
+  }
+
+  // ユーザー情報を更新する
+  @PostMapping("users/{userId}")
+  // urlからuserIdを受け取り、ビューから送られてきたデータをuserという名前でuserEditFormに紐づける。
+  public String updateUser(@PathVariable("userId") Integer userId, @ModelAttribute("user") UserEditForm userEditForm, Model model) {
+    // 取得したidのuser情報をとってきてuserに格納する（idでdb検索するメソッドはリポジトリに記載）
+    UserEntity user = userRepository.findById(userId);
+    // そのuserに、フォームから送られてきたnameとemailをセットする
+    user.setName(userEditForm.getName());
+    user.setEmail(userEditForm.getEmail());
+
+    // 成功すればupdate処理を実行する。引数はuser
+    try {
+      userRepository.update(user);
+    } catch (Exception e) {
+      System.out.println("エラー：" + e);
+      model.addAttribute("user", userEditForm);
+      return "users/edit";
+    }
+    return "redirect:/";
   }
 }
--- src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Update;
 
 import in.tech_camp.chat_app.entity.UserEntity;
 
@@ -17,4 +18,11 @@ public interface UserRepository {
   // emailでユーザーを検索するメソッド
   @Select("SELECT * FROM users WHERE email = #{email}")
   UserEntity findByEmail(String email);
+
+  // idでユーザーを検索するメソッド
+  @Select("SELECT * FROM users WHERE id = #{id}")
+  UserEntity findById(Integer id);
+
+  @Update("UPDATE users SET name = #{name}, email = #{email} WHERE id = #{id}")
+  void update(UserEntity user);
 }
